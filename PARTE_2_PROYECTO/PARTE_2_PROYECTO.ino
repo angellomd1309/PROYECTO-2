8cbc3daa1332bda9434bbeb76e4fcba5e69b7198
Angello Gabriel Mansilla Dieguez <127903674+angellomd1309@users.noreply.github.com>
2023-05-25 17:08:39 -0500

    Update PARTE_2_PROYECTO.ino

--- PARTE_2_PROYECTO/PARTE_2_PROYECTO.ino
+++ PARTE_2_PROYECTO/PARTE_2_PROYECTO.ino
@@ -16,36 +16,34 @@ PRACTICA 4 DISEÑO DE FIRMWARE
 #include <OneWire.h>           
 #include <DallasTemperature.h> 
 Servo miServo;   
-const int tecladoFilas = 4;       // Número de filas del teclado
-const int tecladoColumnas = 4;    // Número de columnas del teclado
-char teclas[tecladoFilas][tecladoColumnas] = {     // Definición de las teclas del teclado
+const int tecladoFilas = 4;       // numero de fila
+const int tecladoColumnas = 4;    // numero de columnas
+char teclas[tecladoFilas][tecladoColumnas] = { 
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-byte pinFilas[tecladoFilas] = {2};              // Pines conectados a las filas
-byte pinColumnas[tecladoColumnas] = {3};  // Pines conectados a las columnas
+byte pinFilas[tecladoFilas] = {2};              // pines de filas
+byte pinColumnas[tecladoColumnas] = {3};  // pines de columnas
+const int pinBoton = 8;      
+const int pinServo = 9;     
+const int pinVentilador = 10; 
 
-const int pinBoton = 8;       // Pin conectado al botón
-const int pinServo = 9;       // Pin conectado al servo
-const int pinVentilador = 10; // Pin conectado al ventilador
+const int pinSensorTemperatura = 6; 
+OneWire oneWire(pinSensorTemperatura);               // creamos variable one wire
+DallasTemperature sensors(&oneWire);                 // creamos variable dalas temperature.
 
-const int pinSensorTemperatura = 6; // Pin conectado al sensor de temperatura
-OneWire oneWire(pinSensorTemperatura);               // Crea un objeto OneWire
-DallasTemperature sensors(&oneWire);                 // Crea un objeto DallasTemperature
+const int pinTrigger = 11;    
+const int pinEcho = 12;       
 
-const int pinTrigger = 11;    // Pin conectado al Trigger del sensor ultrasónico
-const int pinEcho = 12;       // Pin conectado al Echo del sensor ultrasónico
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Dirección I2C del LCD y número de columnas y filas
+boolean estadoServo = false;  
+boolean estadoBoton = false;  
+boolean estadoVentilador = false; 
 
-boolean estadoServo = false;   // Estado actual del servo
-boolean estadoBoton = false;  // Estado actual del botón
-boolean estadoVentilador = false; // Estado actual del ventilador
-
-void setup() {
-  // Configuración del teclado
+void setup() {  // Configuración del teclado
   for (byte fila = 0; fila < tecladoFilas; fila++) {
     pinMode(pinFilas[fila], OUTPUT);
     digitalWrite(pinFilas[fila], HIGH);
@@ -53,26 +51,19 @@ void setup() {
   for (byte columna = 0; columna < tecladoColumnas; columna++) {
     pinMode(pinColumnas[columna], INPUT_PULLUP);
   }
-
-  // Configuración del botón
   pinMode(pinBoton, INPUT_PULLUP);
 
-  // Configuración del servo
   miServo.attach(pinServo);
   miServo.write(0);
 
-  // Configuración del ventilador
   pinMode(pinVentilador, OUTPUT);
   digitalWrite(pinVentilador, LOW);
 
-  // Configuración del sensor de temperatura
+
   sensors.begin();
 
-  // Configuración del sensor ultrasónico
   pinMode(pinTrigger, OUTPUT);
   pinMode(pinEcho, INPUT);
-
-  // Configuración del LCD
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
   lcd.print("Temp: --- C");
@@ -81,7 +72,7 @@ void setup() {
 }
 
 void loop() {
-  // Lectura del teclado
+  
   char teclaPulsada = leerTeclado();
   if (teclaPulsada == 'A') {
     if (estadoServo) {
@@ -93,7 +84,7 @@ void loop() {
     }
   }
 
-  // Lectura del botón
+
   if (digitalRead(pinBoton) == LOW) {
     if (!estadoBoton) {
       if (estadoServo) {
@@ -110,11 +101,11 @@ void loop() {
     estadoBoton = false;
   }
 
-  // Lectura de la temperatura
+
   sensors.requestTemperatures();
   float temperatura = sensors.getTempCByIndex(0);
 
-  // Control del ventilador
+ 
   if (temperatura > 25) {
     digitalWrite(pinVentilador, HIGH);
     estadoVentilador = true;
@@ -123,12 +114,12 @@ void loop() {
     estadoVentilador = false;
   }
 
-  // Lectura del sensor ultrasónico
+  
   float distancia = leerSensorUltrasonico();
 
-  // Actualización del LCD
+
   lcd.setCursor(6, 0);
-  lcd.print("   ");  // Borra el valor anterior
+  lcd.print("   ");  
   lcd.setCursor(6, 0);
   lcd.print(temperatura);
   lcd.setCursor(6, 1);
@@ -144,15 +135,15 @@ char leerTeclado() {
     digitalWrite(pinFilas[fila], LOW);
     for (byte columna = 0; columna < tecladoColumnas; columna++) {
       if (digitalRead(pinColumnas[columna]) == LOW) {
-        delay(20);  // Debounce
-        while (digitalRead(pinColumnas[columna]) == LOW);  // Espera a que se suelte la tecla
+        delay(20);  // Anti rebote
+        while (digitalRead(pinColumnas[columna]) == LOW); 
         digitalWrite(pinFilas[fila], HIGH);
         return teclas[fila][columna];
       }
     }
     digitalWrite(pinFilas[fila], HIGH);
   }
-  return '\0';  // No se ha pulsado ninguna tecla
+  return '\0';  // No se ha pulsado ninguna tecla, en el matricial.
 }
 
 float leerSensorUltrasonico() {
@@ -164,7 +155,7 @@ float leerSensorUltrasonico() {
   long duracion = pulseIn(pinEcho, HIGH);
   float distancia = duracion * 0.034 / 2;
   if (distancia < 2 || distancia > 400) {
-    return 0;  // Fuera de rango
+    return 0; 
   } else {
     return distancia;
   }
